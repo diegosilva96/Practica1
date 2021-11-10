7d3b601742e03391322d453c62becd2e4cda132e
diegosilva96 <92829873+diegosilva96@users.noreply.github.com>
2021-11-10 11:41:38 -0500

    Singgleton

--- src/primerapractica/Apostador.java
+++ src/primerapractica/Apostador.java
@@ -3,7 +3,18 @@ package primerapractica;
 
 public class Apostador {
     private String nombre;
+    private static Apostador apostador;
     
+    private Apostador(){
+        
+    }
+    public static Apostador getInstance(){
+        if(apostador==null){
+            apostador=new Apostador();
+        }
+        return apostador;
+    }
+
 
     public String getNombre() {
         return nombre;
